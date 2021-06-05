c99ac6c28e8594cdb08082c4ca3990525640866c
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-05 22:33:14 +0530

    Resolve issue related to import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
-import Header from "../components/header";
+import Header from "../components/Header";
 import ProductFeed from "../components/ProductFeed";
 
 export default function Home({ products }) {
