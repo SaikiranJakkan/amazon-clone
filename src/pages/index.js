bc830ac2e8cd283ed08d90c72445f49909cdbb89
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-05 22:08:02 +0530

    Integrate `FakeStoreAPI`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
       </Head>
 
-      {/* Header */}
-      <Header></Header>
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />   
+        <ProductFeed products={products}/>
+      </main>   
     </div>
   );
+}
+
+export async function getServerSideProps(context){
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    }
+  }
 }
