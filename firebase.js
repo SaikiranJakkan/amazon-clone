5b41703ec4035cab8979646c0212b46460a13205
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-06 02:25:51 +0530

    Add `firebase.js`

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqpBGCnK2E7Svlt7_ehM_DGhIEpa7giD4",
+    authDomain: "fir-f5bfa.firebaseapp.com",
+    projectId: "fir-f5bfa",
+    storageBucket: "fir-f5bfa.appspot.com",
+    messagingSenderId: "1096672723548",
+    appId: "1:1096672723548:web:691458783fe6f2a744dc99"
+};
