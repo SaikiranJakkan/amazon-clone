55c8b84968ad31022ced5e8cc8f5e632d60fbd8f
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-07 01:29:44 +0530

    Integrate routing using `useRouter` hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,23 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline"
 import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { selectItems } from "../slices/basketSlice";
+import { useSelector } from "react-redux";
 
 function Header() {
   const [session] = useSession();
+  const router = useRouter();
+  //useSelector to take info from the global store
+  const items = useSelector(selectItems);
+
   return (
     <header>
       {/* top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image 
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -42,8 +50,10 @@ function Header() {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative flex items-center link">
-            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded">0</span>
+          <div onClick={() => router.push('checkout')} className="relative flex items-center link">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded">
+              {items.length}
+            </span>
             <ShoppingCartIcon className="h-10" />    
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
           </div>
