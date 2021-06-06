d0f226fd40520767f901a2a50e09b83f98adef3b
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-07 01:32:26 +0530

    Integrate `addItemtoBasket` feature

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,48 +2,72 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
-    const [rating] = useState( 
-        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
-    );
+  const dispatch = useDispatch();
 
-    const [hasPrime] = useState(Math.random < 0.5)
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
 
-    return (
-        <div className="relative flex flex-col m-5 bg-white z-30 p-10">
-            <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+  const [hasPrime] = useState(Math.random() < 0.5);
 
-            <Image src={image} height={200} width={200} objectFit="contain" />
-            <h4 className="my-3">{title}</h4>
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
 
-            <div className="flex">
-            {Array(rating)
-                .fill()
-                .map(( _ , i)=>(
-                    <StarIcon className="h-5 text-yellow-500"/>
-                 ) )}
-            </div>
+    //Sending the product as an action to the REDUX store (basket slice)
+    dispatch(addToBasket(product));
+  };
 
-            <p className="text-xs my-2 line-clamp-2">{description}</p>
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
 
-            <div className="mb-5">
-                <Currency quantity={price}/>
-            </div>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4 className="my-3">{title}</h4>
 
-            {hasPrime && (
-                <div className="flex item-center space-x-2 -mt-5">
-                    <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
-                    <p className="text-xs text-gray-500">"FREE Next-day Delivery</p>
-                </div>
-            )}
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
 
-            <button className="mt-auto button">Add to basket</button>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div className="mb-5">
+        <Currency quantity={price} />
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
         </div>
-    )
+      )}
+
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to basket
+      </button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
