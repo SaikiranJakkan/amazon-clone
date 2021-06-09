916dee0f9ca2a7f8d54276ec57ca7784788d3441
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-10 00:10:16 +0530

    Integrate `subtotal` section

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,53 +1,78 @@
-import Image from "next/image"
+import Image from "next/image";
 import Header from "../components/Header";
 import CheckoutProduct from "../components/CheckoutProduct";
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice"
+import { selectItems, selectTotal } from "../slices/basketSlice";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/client";
 
 function Checkout() {
-const items = useSelector(selectItems);
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const [session] = useSession();
 
-    return (
-        <div className="bg-gray-100">
-            <Header />
+  return (
+    <div className="bg-gray-100">
+      <Header />
 
-            <main className="lg:flex max-w-screen-xl mx-auto">
-                {/* left */}
-                <div className="flex-grow m-5 shadow-sm">
-                    <Image 
-                        src="https://links.papareact.com/ikj"
-                        width={1020}
-                        height={250}
-                        objectFit="contain"
-                    />
+      <main className="lg:flex max-w-screen-xl mx-auto">
+        {/* left */}
+        <div className="flex-grow m-5 shadow-sm">
+          <Image
+            src="https://links.papareact.com/ikj"
+            width={1020}
+            height={250}
+            objectFit="contain"
+          />
 
-                    <div className="flex flex-col p-5 space-y-10 bg-white">
-                        <h1 className="text-3xl border-b pb-4">
-                            {items.length === 0 ? "Your Amazon basket is empty" : "Your Shopping Basket"}  
-                        </h1>
+          <div className="flex flex-col p-5 space-y-10 bg-white">
+            <h1 className="text-3xl border-b pb-4">
+              {items.length === 0
+                ? "Your Amazon basket is empty"
+                : "Your Shopping Basket"}
+            </h1>
 
-                        {items.map((item, i) => (
-                            <CheckoutProduct 
-                                key={i}
-                                id={item.id}
-                                title={item.title}
-                                price={item.price}
-                                description={item.description}
-                                category={item.category}
-                                image={item.image}
-                                hasPrime ={item.hasPrime}
-                            />
-                        ))}
-                    </div>
-                </div>
-
-                {/* right */}
-                <div>
-                    
-                </div>
-            </main>
+            {items.map((item, i) => (
+              <CheckoutProduct
+                key={i}
+                id={item.id}
+                title={item.title}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                image={item.image}
+                hasPrime={item.hasPrime}
+              />
+            ))}
+          </div>
         </div>
-    )
+
+        {/* right */}
+        <div className="flex flex-col bg-white shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowarap">
+                Subtotal ({items.length} items):{" "}
+                <span className="font-bold">
+                    <Currency quantity={total} />
+                </span>
+              </h2>
+
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+              </button>
+            </>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
