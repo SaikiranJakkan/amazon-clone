8dff8e53496c1af73ef1784956a4f06791cc5e2a
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-11 18:59:55 +0530

    Add routing for `orders.js`

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,10 @@ function Header() {
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
 
-          <div className="link">
+          <div 
+            onClick={() => router.push("/orders")}  
+            className="link"
+          >
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
