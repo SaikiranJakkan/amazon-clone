08f9c619bd6397157eba21d90fe9a3ab26b64a31
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-11 18:38:49 +0530

    Add `createCheckoutSession` function

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,32 @@ import { useSelector } from "react-redux";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key)
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    //Call the backend to create a checkout session 
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    })
+
+    //Redirect user/customer to Stripe Checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id
+    })
+
+    if(result.error)
+      alert(result.error.message);
+  }
 
   return (
     <div className="bg-gray-100">
@@ -48,7 +69,7 @@ function Checkout() {
         </div>
 
         {/* right */}
-        <div className="flex flex-col bg-white shadow-md">
+        <div className="flex flex-col p-5 space-y-10 bg-white shadow-md">
           {items.length > 0 && (
             <>
               <h2 className="whitespace-nowarap">
@@ -59,6 +80,8 @@ function Checkout() {
               </h2>
 
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
