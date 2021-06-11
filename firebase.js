a0ce23a8999fb8e1edb06a1ca0f072e4b87b6079
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-11 18:55:20 +0530

    Add connection to `cloud firestore`

--- firebase.js
+++ firebase.js
@@ -1,10 +1,17 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqpBGCnK2E7Svlt7_ehM_DGhIEpa7giD4",
-    authDomain: "fir-f5bfa.firebaseapp.com",
-    projectId: "fir-f5bfa",
-    storageBucket: "fir-f5bfa.appspot.com",
-    messagingSenderId: "1096672723548",
-    appId: "1:1096672723548:web:691458783fe6f2a744dc99"
+  apiKey: "AIzaSyCqpBGCnK2E7Svlt7_ehM_DGhIEpa7giD4",
+  authDomain: "fir-f5bfa.firebaseapp.com",
+  projectId: "fir-f5bfa",
+  storageBucket: "fir-f5bfa.appspot.com",
+  messagingSenderId: "1096672723548",
+  appId: "1:1096672723548:web:691458783fe6f2a744dc99",
 };
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
