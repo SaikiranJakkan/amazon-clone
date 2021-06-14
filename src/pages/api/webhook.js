35bc702e89263c1b401fec4b7c951dda8d342583
Saikiran Jakkan <saikiran.jakkan@gmail.com>
2021-06-14 14:50:45 +0530

    Set up env variables for `permissions.json`

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -2,8 +2,18 @@ import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
 // Secure a connection to FIREBASE from the backend
-
 const serviceAccount = require("../../../permissions.json");
+serviceAccount.type = process.env.FIREBASE_ADMIN_TYPE;
+serviceAccount.project_id = process.env.FIREBASE_ADMIN_PROJECT_ID;
+serviceAccount.private_key_id = process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID;
+serviceAccount.private_key = process.env.FIREBASE_ADMIN_PRIVATE_KEY;
+serviceAccount.client_email = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
+serviceAccount.client_id = process.env.FIREBASE_ADMIN_CLIENT_ID;
+serviceAccount.auth_uri = process.env.FIREBASE_AUTH_URI;
+serviceAccount.token_uri = process.env.FIREBASE_ADMIN_TOKEN_URI;
+serviceAccount.auth_provider_x509_cert_url = process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL;
+serviceAccount.client_x509_cert_url = process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL;
+
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
